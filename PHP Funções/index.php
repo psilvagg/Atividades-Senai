5c52d9eff48ba875f25a30710801e5508280bf17
Pedro <106033591+psilvagg@users.noreply.github.com>
2024-09-06 20:52:30 -0300

    Funções PHP

---
+++ PHP Funções/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade</title>
+</head>
+
+<body>
+    <?php
+    $valor = $_POST['valor'] ?? '';
+    function calc($valor)
+    {
+        $total = ($valor - 32) * 5 / 9;
+        echo "<h3>$total</h3>";
+
+        return $total;
+    }
+    ?>
+    <form action="index.php" method="post" name="">
+        <h2>Conversão</h2>
+        <input type="number" placeholder="Insira um número" name="valor"><br><br>
+        <input type="submit" value="Calcular">
+        <?php
+        calc($valor);
+        ?>
+    </form>
+
+
+</body>
+
+
+
+
+</html>
