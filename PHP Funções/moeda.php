d9cc56160a5d1f5eb544dcf408b39c1f723d6738
Pedro <106033591+psilvagg@users.noreply.github.com>
2024-09-06 21:01:45 -0300

    Dolar para Real

---
+++ PHP Funções/moeda.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dolar para Real</title>
+</head>
+
+<body>
+    <?php
+
+
+
+
+    ?>
+    <form action="moeda.php">
+        <h1>De dolar para real</h1>
+        <h2>Cotação do dolar: R$1.81</h2>
+        <input type="number" placeholder="Insira o valor em R$"><br><br>
+        <input type="submit" value="Converter">
+    </form>
+
+</body>
+
+</html>
